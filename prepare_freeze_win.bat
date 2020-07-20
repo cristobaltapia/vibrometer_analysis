256407eb9a08b771a567937f4e0b16e5616b50ac
Cristóbal Tapia Camú <crtapia@gmail.com>
2020-07-20 11:31:33 +0200

    fix: I have no idea about batch fiels

--- prepare_freeze_win.bat
+++ prepare_freeze_win.bat
@@ -1,4 +1,4 @@
 echo Updating sounddevice.py ...
 copy .\portaudio\sounddevice_custom.py sounddevice.py .\venv\Lib\site-packages\sounddevice.py /y
-copy .\portaudio\libportaudio32bit.dll .\venv\Lib\site-packages\libportaudio32bit.dll /y
-copy .\portaudio\libportaudio64bit.dll .\venv\Lib\site-packages\libportaudio64bit.dll /y
+copy .\portaudio\libportaudio32bit.dll .\venv\Lib\site-packages\bin\libportaudio32bit.dll /y
+copy .\portaudio\libportaudio64bit.dll .\venv\Lib\site-packages\bin\libportaudio64bit.dll /y
