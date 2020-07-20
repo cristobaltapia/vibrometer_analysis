4d93eae9c57f57f15e1b70f1993dab75fc92686a
Cristóbal Tapia Camú <crtapia@gmail.com>
2020-07-20 14:47:22 +0200

    Update PyInstaller hooks

--- src/hooks/hook-scipy.signal.py
+++ src/hooks/hook-scipy.signal.py
@@ -1,1 +1,16 @@
-hiddenimports = ['scipy.linalg.cython_blas', 'scipy.linalg.cython_lapack']
+import os
+import glob
+from PyInstaller.utils.hooks import get_module_file_attribute
+from PyInstaller.compat import is_win
+
+binaries = []
+
+if is_win:
+    dll_glob = os.path.join(os.path.dirname(
+        get_module_file_attribute('scipy')), '.libs', "*.dll")
+    if glob.glob(dll_glob):
+        binaries.append((dll_glob, "."))
+
+# collect library-wide utility extension modules
+hiddenimports = ['scipy._lib.%s' % m for m in [
+    'messagestream', "_ccallback_c", "_fpumode"]]
