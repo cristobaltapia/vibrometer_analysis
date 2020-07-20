9f0c838c3ccce8ac4a5a31929a1a04463e8e1b56
Cristóbal Tapia Camú <crtapia@gmail.com>
2020-07-20 10:44:52 +0200

    fix: portaudio in windows freeze

--- prepare_freeze_win.bat
+++ prepare_freeze_win.bat
@@ -1,4 +1,4 @@
 echo Updating sounddevice.py ...
-copy .\sounddevice.py .\venv\Lib\site-packages\sounddevice.py /y
+copy .\portaudio\sounddevice_custom.py sounddevice.py .\venv\Lib\site-packages\sounddevice.py /y
 copy .\libportaudio32bit.dll .\venv\Lib\site-packages\libportaudio32bit.dll /y
 copy .\libportaudio64bit.dll .\venv\Lib\site-packages\libportaudio64bit.dll /y
